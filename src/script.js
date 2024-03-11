dc8a5017c1d06bd897ff8a2c61150bd45be15757
Marko Jokinen <grumpy.devoloper@outlook.com>
2024-03-11 18:22:53 +0700

    speed isights gives error on build removed

--- src/script.js
+++ src/script.js
@@ -1,9 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
-import { injectSpeedInsights } from "@vercel/speed-insights/*";
-
-injectSpeedInsights();
 
 /**
  * Base
